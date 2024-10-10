0aaed2d7612ee29344fed368253c1bdd45f090fe
“Elena <elemoser@gmail.com>
2024-10-10 15:47:25 +0200

    Refactor project to improve style

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,6 +8,7 @@ import {
   Link,
   Stack,
   Text,
+  CardFooter,
 } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
@@ -24,35 +25,31 @@ const Project = ({ children, link, repo, name, image }: Props) => {
     <Box borderRadius={10} overflow="hidden">
       <Card>
         <CardBody>
-          {image.length === 0 ? (
-            <Image
-              src="gibbresh.png"
-              fallbackSrc="https://via.placeholder.com/200"
-            />
-          ) : (
-            <Image src={"https://elemoser.github.io/Images/" + image} />
-          )}
+          <Image
+            borderRadius="lg"
+            src={
+              image.length === 0
+                ? "https://via.placeholder.com/200"
+                : "https://elemoser.github.io/Images/" + image
+            }
+          />
           <Stack mt="6" spacing="3">
             <Heading size="md">{name}</Heading>
-            <Text>{children}</Text>
-            {repo.length === 0 ? (
-              <Text>(Private Repository)</Text>
-            ) : (
-              <Link href={repo} target="_blank">
-                <Button colorScheme="teal" variant="solid">
-                  View repo
-                </Button>
-              </Link>
-            )}
-            {link.length === 0 ? null : (
-              <Link href={link} target="_blank">
-                <Button colorScheme="teal" variant="solid">
-                  View site
-                </Button>
-              </Link>
-            )}
+            <Text fontSize="md">{children}</Text>
           </Stack>
         </CardBody>
+        <CardFooter gap={5} flexWrap="wrap">
+          {repo.length > 0 ? (
+            <Link href={repo} target="_blank">
+              <Button variant="outline">Repo</Button>
+            </Link>
+          ) : null}
+          {link.length === 0 ? null : (
+            <Link href={link} target="_blank">
+              <Button variant="outline">Link</Button>
+            </Link>
+          )}
+        </CardFooter>
       </Card>
     </Box>
   );
